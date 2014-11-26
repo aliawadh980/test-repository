d048966344f04148052fb7f7e43d10269a310323
aliawadh980 <ali980@gmail.com>
2014-11-26 19:14:35 +0300

    Create Test_file_1.php
    This is the extended comment about the file Test_file_1.php
    And by this maybe we don't have to add comments description in the code header.

---
+++ Test_file_1.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Test file 1</title>
+  </head>
+  <body>
+    <h1>Test file 1</h1>
+    <?php
+      echo "<p>Test from inside PHP.</p>";
+    ?.
+  </body>
+</html>
